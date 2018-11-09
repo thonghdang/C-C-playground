e2a49f990bf2e7282e6afe821116bd0640c36bd6
Thong Dang <danghoangthong274@gmail.com>
2018-11-08 22:40:02 -0600

    Modified main module.

--- Project-2/Spell-checker/main.c
+++ Project-2/Spell-checker/main.c
@@ -1,80 +1,76 @@
 
-/*
- * CS 250
+/* CS 250
  * Project 2: A Simple Spell-Checker
  * Author: Thong Dang
  * Date: November 7th, 2018
  * Description: This program is a simple spell check user input.
  */
 
-/* Pre-processor directives */
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
 #include "dictionary.h"
 
-
-/* Declaring constants */
-// #define MAX_LINE_LENGTH 1024
-// #define MAX_WORD_SIZE 100
-// #define MIN(a,b) (((a)<(b))?(a):(b))
-
-
-/* Declaring function prototype */
-//unsigned int distance (const char *a, const char *b);
-
+int number_of_words;
+char dictionary[MAX_LINES][MAX_WORD_LEN];
 
 /* Main module */
 int main ()
 {
-  extern int words_num;
-  extern char dictionary[MAX_LINES][MAX_WORD_LEN];
 
-  // Create the dictionary in a 2D array
+  // Read the dictionary into array
   make_dict ("dictionary.txt");
-  char line[MAX_LINE_LENGTH];
+
+  char input[MAX_LINE_LENGTH];
   printf ("Enter a string: ");
 
-  // Take user's input
-  fgets (line, MAX_LINE_LENGTH, stdin);
-  line[strlen(line)-1] = '\0';
-  char * word;
-  word = strtok (line, " ");
-  int min_dist;
+  // Prompt user input and store into array
+  fgets (input, MAX_LINE_LENGTH, stdin);
+  input[strlen(input)-1] = '\0';
+
+  char * unchecked_words;
+  int min_distance;
   int i;
-  char*correct_word;
+  char * correct_words;
+
+  // Extract word by word from user input.
+  unchecked_words = strtok (input, " ");
+
   
  
-  while (word != NULL)
-    { 
-      min_dist= MAX_WORD_SIZE;
-      
-      for (i=0; i<words_num; i++) {
-                
-	if ((distance(dictionary[i], word))==0)
-	  {
-	    correct_word=word;
-	    
-	    break;
-	  } 
-	else
-	{
-	    int dist = distance (word, dictionary[i]);
-	    
-	    if (dist < min_dist) {
-	      min_dist = dist;
-	      correct_word=dictionary[i];
+  while (unchecked_words != NULL) 
+  { 
+    min_distance = MAX_WORD_SIZE;
+    for (i=0; i < number_of_words; i++) 
+    {
+      if ((distance(dictionary[i], unchecked_words))==0) 
+      {
+        correct_words= unchecked_words;
+        break;
+      } 
+      else 
+      {
+        int dist = distance (unchecked_words, dictionary[i]);
+
+        if (dist < min_distance) {
+	        min_distance = dist;
+	        correct_words =dictionary[i];
+        }
 	    }
-	  }
-  
-      }
-      if (correct_word == word) {
-	printf("%s ",correct_word);
-      } else {
-	printf ("*%s* ", correct_word);
-      }
-      word = strtok (NULL, " ");
     }
+
+    if (correct_words == unchecked_words)
+    {
+      printf("%s ",correct_words);
+    } 
+    else 
+    {
+      printf ("*%s* ", correct_words);
+    }
+      
+    // Get to the next words.
+    unchecked_words = strtok (NULL, " ");
+  }
 }
 
 
